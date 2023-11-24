a244336fc26783be9b3b99cddb90acaf8390b511
Khaela Fortunela <72397657+fortunelagit@users.noreply.github.com>
2023-11-24 10:29:11 +0700

    Update req.py

--- req.py
+++ req.py
@@ -1,6 +1,6 @@
 import requests
 
 resp = requests.post("http://127.0.0.1:5000", 
-                     files={'file': open('./Abyssinian_24', 'rb')})
+                     files={'file': open('./Abyssinian_24.jpg', 'rb')})
 
 print(resp.json())
